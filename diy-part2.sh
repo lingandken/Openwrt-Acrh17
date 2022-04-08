6d3ca5c9b6e9f84a2f0f2d57504d1922bc95b78f
lingandken <64186033+lingandken@users.noreply.github.com>
2022-04-08 09:38:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,15 +25,5 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lea
 #sed -i "s/OpenWrt /PandoraBox /g" package/lean/default-settings/files/zzz-default-settings
 
 #### 稳定版修改R21xxx+自己的名字
-sed -i 's/R22.2.2/R22.3.15_By J/g' package/lean/default-settings/files/zzz-default-settings
-
-sed -i 's/set wireless.default_radio${devidx}.ssid=OpenWrt/set wireless.default_radio0.ssid=Home_5G/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-
-sed -i '/set wireless.default_radio0.ssid=RT-ACRH17/a\ set wireless.default_radio1.ssid=Home_2G' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-
-#sed -i 's/"Frp 内网穿透"/"超级服务器外网穿透"/g' feeds/luci/applications/luci-app-frpc/po/zh-cn/frp.po
-
-sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' feeds/luci/applications/luci-app-turboacc/po/zh-cn/turboacc.po
-
-sed -i 's/"CPU 性能优化调节"/"处理器性能优化调节"/g' feeds/luci/applications/luci-app-cpufreq/po/zh-cn/cpufreq.po
+sed -i 's/R22.4.1/Build $(TZ=UTC-8 date "+%Y.%m.%d") By JJ/g' package/lean/default-settings/files/zzz-default-settings
 
